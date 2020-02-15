48cb616b4de5c52b46168166298003eaba1fbead
andrewcaunes <52610242+andrewcaunes@users.noreply.github.com>
2020-02-15 12:14:32 +0100

    Add files via upload

---
+++ include/RD_derivative_estimator.h
@@ -0,0 +1,16 @@
+#ifndef RD_DERIVATIVE_ESTIMATOR_H
+#define RD_DERIVATIVE_ESTIMATOR_H
+
+
+class RD_derivative_estimator
+{
+    public:
+        RD_derivative_estimator(double Y[], double X[], int n);
+        virtual ~RD_derivative_estimator();
+
+    protected:
+
+    private:
+};
+
+#endif // RD_DERIVATIVE_ESTIMATOR_H
